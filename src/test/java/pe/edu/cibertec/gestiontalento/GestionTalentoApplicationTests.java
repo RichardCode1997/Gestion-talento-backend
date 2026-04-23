ca50051035360c25f5bbc4ca2982dbeb0634ed95
ericksson <ericksson12021997@gmail.com>
2026-04-23 03:27:03 -0500

    config: Java 21, Spring Boot 3.2.0, H2 para tests con perfil test

--- src/test/java/pe/edu/cibertec/gestiontalento/GestionTalentoApplicationTests.java
+++ src/test/java/pe/edu/cibertec/gestiontalento/GestionTalentoApplicationTests.java
@@ -2,8 +2,10 @@ package pe.edu.cibertec.gestiontalento;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestionTalentoApplicationTests {
 
 	@Test
